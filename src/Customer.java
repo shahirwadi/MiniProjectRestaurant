94ceb69d7b316159c8db8e0eff1d64eb3ec14dc3
Saifdn <saifudinahmad1234@gmail.com>
2024-06-13 16:37:16 +0800

    Customer class created

---
+++ src/Customer.java
@@ -0,0 +1,18 @@
+package src;
+
+public class Customer extends User{
+    
+    private int custID;
+    private Review review;
+
+    public Customer(String name, String phone, String email, int custID, Review review){
+        super(name, phone, email);
+        this.custID = custID;
+        this.review = review;
+    }
+
+    public int getCustomerID(){
+        return custID;
+    }
+
+}
