039dfab626bf8267d3ef0edee4741fce09311eca
Saifdn <saifudinahmad1234@gmail.com>
2024-06-14 03:45:54 +0800

    Restaurant class created

---
+++ Restaurant.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+import src.Customer;
+
+public class Restaurant {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        
+
+        scanner.close();
+    }
+
+}
